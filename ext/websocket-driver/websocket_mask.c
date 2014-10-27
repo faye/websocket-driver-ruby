648e0c4cd1b4a2e9fb0129181b10fef6d45d6a5e
James Coglan <jcoglan@gmail.com>
2014-10-27 21:15:48 +0000

    Fix C compilation warnings about interleaved declarations and code.

--- ext/websocket-driver/websocket_mask.c
+++ ext/websocket-driver/websocket_mask.c
@@ -13,19 +13,20 @@ void Init_websocket_mask() {
 }
 
 VALUE method_websocket_mask(VALUE self, VALUE payload, VALUE mask) {
+  int n, i, p, m;
+  int mask_array[4];
+  VALUE unmasked;
+
   if (mask == Qnil || RARRAY_LEN(mask) == 0) {
     return payload;
   }
 
-  int n = RARRAY_LEN(payload), i, p, m;
-  VALUE unmasked = rb_ary_new2(n);
+  n = RARRAY_LEN(payload);
+  unmasked = rb_ary_new2(n);
 
-  int mask_array[] = {
-    NUM2INT(rb_ary_entry(mask, 0)),
-    NUM2INT(rb_ary_entry(mask, 1)),
-    NUM2INT(rb_ary_entry(mask, 2)),
-    NUM2INT(rb_ary_entry(mask, 3))
-  };
+  for (i = 0; i < 4; i++) {
+    mask_array[i] = NUM2INT(rb_ary_entry(mask, i));
+  }
 
   for (i = 0; i < n; i++) {
     p = NUM2INT(rb_ary_entry(payload, i));
